2cc795f2d885342301372fe9bac9be0ecee11c73
Taras <taras.peschenko.90@mail.ru>
2022-12-21 00:38:45 +0300

    Рекурсивный метод, который найдёт сумму натуральных элементов в промежутке от M до N.

---
+++ Task_66/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Project
+{
+    class Program
+    {
+        static void Main(string[] agrs)
+        {
+            void Task69()
+            {
+                //  Задайте значения M и N. Напишите рекурсивный метод, 
+                // который найдёт сумму натуральных элементов в промежутке от M до N.
+
+                // M = 1; N = 15 -> 120
+                // M = 4; N = 8. -> 30
+                int sum = 0;
+                PrintNumbers();
+
+
+                void PrintNumbers(int M = 1, int N = 5)
+                {
+                    if(M > N) return;
+                    sum += M;
+                    M++;
+                    PrintNumbers(M, N);
+                }
+
+            Console.Write(sum);  
+                 
+                
+            }
+
+            Task69();
+        }   
+    }
+}
