f942db46fa6ebbc5133f41275baa0174eb66406f
Taras <taras.peschenko.90@mail.ru>
2022-12-21 20:34:55 +0300

    Функция Аккермана, с помощью рекурсии.

---
+++ Task_68/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Project
+{
+    class Program
+    {
+        static void Main(string[] agrs)
+        {
+            void Task68()
+            {
+                // Задача 68: Напишите программу вычисления функции Аккермана 
+                // с помощью рекурсии. Даны два неотрицательных числа m и n.
+                // m = 2, n = 3 -> A(m,n) = 9
+                // m = 3, n = 2 -> A(m,n) = 29               
+                
+                Console.Write(A(3, 2));
+
+                uint A(uint m, uint n)
+
+                {
+                    if(m==0) return n + 1;
+
+                    else if(n==0) return A(m -1, 1);
+
+                    else return A(m - 1, A(m, n -1));                 
+                }
+                     
+                
+            }
+
+            Task68();
+        }   
+    }
+}
